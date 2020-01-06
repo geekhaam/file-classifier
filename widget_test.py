6b4fb1b25e1ad218f0d9bb4b36cdd830eca9f712
geekhaam <geekhaam@gamil.com>
2020-01-06 22:40:57 +0900

    Add __pycache__/ emit_test.py os_test.py pyuac.py widget_test.py

---
+++ widget_test.py
@@ -0,0 +1,44 @@
+import sys
+from PyQt5.QtWidgets import *
+
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):               
+        
+        self.resize(1000, 600)
+        self.center()
+
+        self.setWindowTitle('file-classifier')    
+        self.show()
+    
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        
+    def closeEvent(self, event):
+        
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()        
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
